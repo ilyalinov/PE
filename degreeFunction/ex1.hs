90df6063b891fd5545f34f7c6f01110c7dc94308
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2016-11-19 01:21:27 +0300

    changed power function and added its generating extension

--- degreeFunction/ex1.hs
+++ degreeFunction/ex1.hs
@@ -1,13 +1,18 @@
+import qualified Data.Text as T
+
 square :: Int -> Int
 square x = x * x
 
 f :: Int -> Int -> Int
-f n x = 
-    if n == 0 then
-        1
-    else if even n then
-        square (f (quot n 2) x)
-    else
-        (f (n - 1) x) * x
+f x 0  = 1
+f x n | even n    = square (f (quot n 2) x)
+      | otherwise = (f (n - 1) x) * x
 
 fSpecialized6 x = square (x * (square x))
+
+
+generatingExtension n = "fSpecialized" ++ show n ++ " x = "++ loop n where
+    loop 0 = "1"
+    loop n | even n    = "(square " ++ loop (n `quot` 2) ++ ")"
+           | otherwise = "(" ++ loop (n - 1) ++ " * x)"
+    
