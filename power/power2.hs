31a87bc137919e98e83e65705d35e084597497b7
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2016-12-07 16:32:02 +0300

    corrections

--- power/power2.hs
+++ power/power2.hs
@@ -4,8 +4,18 @@ data Exp = Exp Int Power
 eval :: Exp -> Int
 eval (Exp x Deg0) = 1
 eval (Exp x (Mult power)) = (eval (Exp x power)) * x
-eval (Exp x (Square power)) = (eval (Exp x power)) * (eval (Exp x power))
+eval (Exp x (Square power)) =  let y = eval (Exp x power) in y * y
 
+main = do
+    putStrLn $ show $ eval test0
+    putStrLn $ show $ eval test1
+    putStrLn $ show $ eval test2
+    where
+    test0 = Exp 3 Deg0
+    test1 = Exp 3 (Mult (Square (Mult Deg0)))    
+    test2 = Exp 3 (Square (Square (Mult Deg0)))
+
+{-
 --3 ^ 0
 test0 :: Exp
 test0 = Exp 3 Deg0
@@ -17,8 +27,6 @@ test1 = Exp 3 (Mult (Square (Mult Deg0)))
 -- (3 ^ 2) ^ 2 = 3 ^ 4
 test2 :: Exp
 test2 = Exp 3 (Square (Square (Mult Deg0)))
-
-main = do
-    putStrLn $ show $ eval test0
-    putStrLn $ show $ eval test1
-    putStrLn $ show $ eval test2
+-}
+    
+    
